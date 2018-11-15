539bbab3c0d809a7e97633c92dc4c37a0b18b6ec
Perry Mitchell <perry@perrymitchell.net>
2018-11-15 22:17:25 +0200

    Add docs

--- source/fs.js
+++ source/fs.js
@@ -8,6 +8,13 @@ function createFsInterface(adapter) {
      * @class DropboxClientFsAdapter
      */
     return {
+        /**
+         * Read the contents of a directory
+         * @param {String} remotePath The remote directory
+         * @param {Object=} options (Not in use)
+         * @param {Function} callback The callback to execute with the results or error
+         * @memberof DropboxClientFsAdapter
+         */
         readdir: (remotePath, options = {}, callback) => {
             let cb = callback,
                 ops = options;
@@ -32,6 +39,13 @@ function createFsInterface(adapter) {
                 })
                 .catch(cb);
         },
+        /**
+         * Read the contents of a file
+         * @param {String} remotePath The remote file path
+         * @param {Object=} options (Not in use)
+         * @param {Function} callback The callback to execute with the results or error
+         * @memberof DropboxClientFsAdapter
+         */
         readFile: (remotePath, options = {}, callback) => {
             let cb = callback;
             if (typeof options === "function") {
@@ -42,6 +56,14 @@ function createFsInterface(adapter) {
                 .then(content => cb(null, content))
                 .catch(cb);
         },
+        /**
+         * Write to a remote file
+         * @param {String} remotePath The remote file path
+         * @param {String|Buffer} data The data to write
+         * @param {Object=} options (Not in use)
+         * @param {Function} callback The callback to execute, possibly with an error
+         * @memberof DropboxClientFsAdapter
+         */
         writeFile: (remotePath, data, options = {}, callback) => {
             let cb = callback;
             if (typeof options === "function") {
