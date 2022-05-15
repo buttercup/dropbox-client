f6ea8eb46c47e533dac674423aff43ecedb321d3
Perry Mitchell <perry@perrymitchell.net>
2022-05-15 14:28:46 +0300

    Setup publish process

--- source/index.ts
+++ source/index.ts
@@ -1,14 +1,3 @@
 export { DropboxClient } from "./client.js";
 export { generateAuthorisationURL } from "./auth.js";
-
-// const {
-//     createClient,
-//     createFsInterface
-// } = require("./client.js");
-// const { generateAuthorisationURL } = require("./auth.js");
-
-// module.exports = {
-//     createClient,
-//     createFsInterface,
-//     generateAuthorisationURL
-// };
+export { DropboxPathInfo } from "./types.js";
